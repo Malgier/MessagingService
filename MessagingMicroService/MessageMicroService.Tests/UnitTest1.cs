e32ae1b3e1cdb9c9538125e656c21e6e70419aee
Malgier <stevo730@hotmail.co.uk>
2017-11-29 15:31:14 +0000

    Tests

---
+++ MessagingMicroService/MessageMicroService.Tests/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+using System.Net;
+
+namespace MessageMicroService.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            FakeResponseHandler fakeResponseHandler = new FakeResponseHandler();
+            // Add hardcoded external service responses
+            fakeResponseHandler.AddFakeResponse(
+                new Uri("http://localhost:54330/api/ProductStore/" + 1234),
+                new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new ObjectContent<>(
+                        new TPRProduct()
+                        {
+                            Name = "ProductName",
+                            Description = "A product description.",
+                            Ean = "1234",
+                            InStock = true,
+                            Price = 0.00,
+                            BrandId = 1,
+                            CategoryId = 2,
+                            Active = true
+                        }, new JsonMediaTypeFormatter())
+                });
+        }
+    }
+}
